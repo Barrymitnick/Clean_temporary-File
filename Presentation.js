298a8441f8a1e8a948750c2dc424f25ba4c1f720
Barrymitnick <131542330+Barrymitnick@users.noreply.github.com>
2023-10-22 23:56:30 +0200

    Create Presentation.js

---
+++ Presentation.js
@@ -0,0 +1,1 @@
+console.log("Ce script fonctionne en utilisant la fonction sha1sum pour calculer l'empreinte numérique de chaque fichier. Deux fichiers sont considérés comme en double si leur empreinte numérique est identique.")
