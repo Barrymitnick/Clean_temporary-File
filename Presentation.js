d504398542b1ca91c62faf1e06953730a78f7c93
Barrymitnick <131542330+Barrymitnick@users.noreply.github.com>
2023-10-23 00:01:10 +0200

    Delete Presentation.js

--- Presentation.js
+++
@@ -1,1 +0,0 @@
-console.log("Ce script fonctionne en utilisant la fonction sha1sum pour calculer l'empreinte numérique de chaque fichier. Deux fichiers sont considérés comme en double si leur empreinte numérique est identique.")
